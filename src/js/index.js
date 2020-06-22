1a7d212d3dabb773c9fc6597f2ddc6838b6abdae
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2020-06-22 13:53:32 +0300

    Completed edit mixin StandartNavBar. Added area surname with possibility change his state

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import 'normalize.css';
 import '../style/style.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const menuElements = document.querySelectorAll('.navbar__link');
-    menuElements[0].classList.add('active');
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuElements = document.querySelectorAll('.navbar__link');
+//     menuElements[0].classList.add('active');
+// });
 console.log(1)
