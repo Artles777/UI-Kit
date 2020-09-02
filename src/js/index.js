ec740f6fb22ce2fe4b0def4751a2fdb5b562c429
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2020-09-02 17:50:49 +0300

    Completed the buttons from file _buttons.pug, small changes entered in the files _inputs.scss and added rules in the file custom.scss

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,23 @@ import '../style/style.scss'
 
 import searchRate from "./modules/searchRate";
 
+// Use only in the UI kit
 window.addEventListener('DOMContentLoaded', () => {
     searchRate();
+    
+    function borderActive(inputSelector) {
+        const input = document.querySelectorAll(inputSelector);
+
+        input.forEach(item => {
+
+            item.addEventListener('focus', () => {
+                item.parentNode.style = 'border: 1px solid rgba(31, 32, 65, 0.5)'
+            });
+            item.addEventListener('blur', () => {
+                item.parentNode.style = ''
+            });
+        })
+    }
+
+    borderActive('.text-field__input');
 });
