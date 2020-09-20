14cfe1e21912cfbb5470a4ec3a0b749dd4cb139c
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2020-09-20 22:42:06 +0300

    Completed container 'Form Elements.pug'. Added files the _advantages.pug, _advantages.scss and _advantages.module.pug. Entering small changes.

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,5 @@ const isMap = () => isEnv() === 'development' ? 'inline' : false;
 
 module.exports = {
     to: path.resolve(__dirname, 'dist'),
-    from: path.resolve((__dirname, './src/style/style.scss')),
+    from: path.resolve(__dirname, './src/style/style.scss'),
 }
